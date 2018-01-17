25e5887337f976935e676afc2406a6208e2dfcc9
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-01-17 16:59:36 +0530

    EVENODD.C
    TO CHECK NUMBER ISEVEN OR ODD

---
+++ EVENODD.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("Enter the an integer:");
+scanf("%d ,number);
+if(number%2==0)
+printf("%d is even",number);
+else
+print(%d is odd",number);
+return0;
+}
