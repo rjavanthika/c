7a22691207c15bb6a8c6b9f631b2e841c2a82453
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-02-05 10:19:44 +0000

    PRIME OR NOT.
    PRINT THE PRIMENUMBER NOR NOT

---
+++ PRIME OR NOT.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int n,i,flag=0;
+printf("enter a positive integer:");
+scanf("%d",&n);
+for (i=2;i<n/2;++i)
+{
+if(n%1==0)
+{
+flag=1;
+break;
+}}
+if(flag==0)
+printf("%d is aprimenumber:",n);
+else
+printf("%d is not a prime number:",n);
+return 0;
+}
