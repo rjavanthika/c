36d53ca0f7fac3c80b91fa7744e61d11a3f225a5
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-02-26 11:11:34 +0000

    SWAP.C
    PRINT THE SWAP 2 NUMBER

---
+++ SWAP.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    printf("Enter b: ");
+    scanf("%d",&b);
+
+    // Swapping process
+
+    a = a - b;
+    b = a+ b;
+    a = b - a;
+
+    printf("\nAfter swapping, a = %d\n", a);
+    printf("After swapping, b = %d", b);
+
+    return 0;
+}
