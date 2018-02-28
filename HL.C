86f32f401de1cc13ba319ed96034ddf2d8df5d2f
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-02-28 11:16:18 +0000

    HL.C
    PRINT THE LOWEST AND HIGHEST INAN ARRAY

---
+++ HL.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main()
+{
+	int val[5],h,l,i;
+	for(i=0;i<5;i++)
+        {
+		printf("\nenter the value-%d: ",i+1);
+		scanf("%d",&val[i]);
+	}
+	l=val[0];
+	h=val[0];
+	for(i=0;i<5;i++)
+        {
+		if(val[i]>h)
+			h=val[i];
+		else
+                {
+			if(val[i]<l)
+				l=val[i];
+		}
+	}
+	printf("\nHIGHEST VALUE IS %d",h);
+	printf("\nLOWEST VALUE IS %d",l);
+	return 0;
+}
