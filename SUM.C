9d8278938cb5e8d8535e7763e27bbc280b6e8d14
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-02-28 11:01:30 +0000

    SUM.C
    PRINT THE SUM OF TWO NUMBER

---
+++ SUM.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main(void) {
+int a=1,b=1,sum;
+printf("enter  the numbers:\n");
+printf("enter the a and b\n");
+scanf("%d %d",&a,&b);
+sum=a+b;
+printf("sum of two value is%d,sum");
+	return 0;
+}
