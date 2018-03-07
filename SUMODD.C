f4356a4089499223b3b2ed7a13cf7ea93a8968fb
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-03-07 11:15:13 +0000

    Create SUMODD.C

---
+++ SUMODD.C
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,sub;
+printf("enter the number a and b");
+scanf("%d",&a);
+scanf("%d",&b);
+sub=a-b;
+printf("\nsum of number is %d",sub);
+
+if(sub%2==0)
+{
+printf("even");
+}
+else
+{
+printf(" odd");
+}
+return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
