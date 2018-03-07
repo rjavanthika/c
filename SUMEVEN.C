38e0eed249e130f06b2e002fdd52986e981bfdd1
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-03-07 10:43:07 +0000

    Create SUMEVEN.C

---
+++ SUMEVEN.C
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,sum;
+printf("enter the number a and b");
+scanf("%d",&a);
+scanf("%d",&b);
+sum=a+b;
+printf("\nsum of number is %d",sum);
+
+if(sum%2==0)
+{
+printf("even");
+}
+else
+{
+printf(" odd");
+}
+return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
