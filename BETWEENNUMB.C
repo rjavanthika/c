d3e3862ecd91d65e788339697e000f5e1c7266b8
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-03-12 10:23:20 +0000

    Create BETWEENNUMB.C

---
+++ BETWEENNUMB.C
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+    int main()
+    {
+    	int n=3,i=1,j=13;
+    	if((n>1)&&(n<13))
+    	{
+    		printf("yes");
+    	}
+    	else
+    	{
+    		printf("no");
+    	}
+retun(0);
+}
