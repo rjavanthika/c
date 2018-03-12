14c6277c999173f2e79c2813bef30e4e628e3583
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-03-12 10:28:41 +0000

    Create VOWELS2.C

---
+++ VOWELS2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char m;
+int isLowercaseVowel,isUppercaseVowel;
+printf("enter aan alphabet:");
+scanf("%s,&m);
+isLowercaseVowel1=(m=='a'\\m=='e'\\m=='i'\\m=='o'\\m=='u')
+isUppercaseVowel1=(m=='A'\\m=='E'\\m=='I'\\m=='O'\\m=='U')
+if(isLowercaseVowel1\\isUppercaseVowel1)
+printf(%s is a vowel",m);
+else
+printf(%s is consonant",m);
+return 0;
+}
