4f151b2eb2dbc47adc6f08ae647cb74107cb815e
rjavanthika <35295048+rjavanthika@users.noreply.github.com>
2018-03-12 10:49:30 +0000

    Create COMPOSITE.C

---
+++ COMPOSITE.C
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+
+int m,i,flag=0;
+
+scanf("%d",&m);
+
+for(i=2;i<=m/2;i++)
+
+{
+
+if(m%i==0)
+
+{
+
+flag=1;
+
+break;
+
+}
+
+}
+
+if(flag==0)
+
+{
+
+printf("\n no");
+
+}
+
+else
+
+{
+
+printf("\n yes");
+
+}
+
+return 0;
+
+}
